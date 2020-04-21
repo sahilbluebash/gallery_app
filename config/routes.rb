dcfe578fb948a7161c97aaa5b61df3f89645e3d9
sahilbluebash <sahil.bluebash@gmail.com>
2020-04-21 19:10:25 +0530

    Tags search

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   root 'imagegalleries#showallimages'
 
 
-  get 'tags/:tag', to: 'imagegalleries#tagged',as: :tag
+ # get 'tags/:tag', to: 'imagegalleries#tagged',as: :tag
   get "/showallimages" => "imagegalleries#showallimages"
+  get '/tagged', to: "imagegalleries#tagged", as: :tagged 
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
