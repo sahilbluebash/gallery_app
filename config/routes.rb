5584b1941944fb6e8afd45e54accadd30099cec8
sahilbluebash <sahil.bluebash@gmail.com>
2020-04-30 13:25:29 +0530

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   
   resources :imagegalleries
-  devise_for :users, :controllers => {:registrations => "registrations"}
   root 'imagegalleries#showallimages'
+  
+  devise_for :users, :controllers => {:registrations => "registrations"}
+ 
 
 
  # get 'tags/:tag', to: 'imagegalleries#tagged',as: :tag
   get "/showallimages" => "imagegalleries#showallimages"
   get '/tagged', to: "imagegalleries#tagged", as: :tagged 
   
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
